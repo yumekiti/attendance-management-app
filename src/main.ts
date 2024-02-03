39aa857f106404d3333ea99a54456c7029a854b0
yumekiti <yumekiti120440@gmail.com>
2024-02-04 00:14:42 +0900

    chore: update main script

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import path from "path";
-import { BrowserWindow, app, ipcMain } from "electron";
+import { BrowserWindow, Menu, Tray, app, ipcMain, nativeImage } from "electron";
 const si = require('systeminformation');
 
+let mainWindow: any = null;
+let tray = null;
+
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     webPreferences: {
       preload: path.resolve(__dirname, "preload.js"),
     },
@@ -11,12 +14,40 @@ const createWindow = () => {
 
   mainWindow.loadFile("dist/index.html");
   // mainWindow.webContents.openDevTools({ mode: "detach" });
+
+  // Close to Hide Window
+  mainWindow.on('close', (event: any) => {
+    event.preventDefault()
+    mainWindow.hide()
+  })
 };
+
+// Single Instance
+const gotTheLock = app.requestSingleInstanceLock()
+if (!gotTheLock) app.exit()
 
 app.whenReady().then(() => {
   ipcMain.handle('system', () => si.system())
+  ipcMain.handle('wifiConnections', () => si.wifiConnections())
 
   createWindow();
+
+  // Tray Icon
+  let iconPath: string;
+  if (process.platform === "win32") iconPath = path.join(__dirname, "./../src/assets/icon.ico");
+  else iconPath = path.join(__dirname, "./../src/assets/icon.png");
+
+  // Tray
+  const trayIcon = nativeImage.createFromPath(iconPath);
+  const contextMenu = Menu.buildFromTemplate([
+    { label: '終了', click: () => app.exit() }
+  ]);
+  tray = new Tray(trayIcon);
+  tray.setToolTip(app.name);
+  tray.setContextMenu(contextMenu);
+  tray.on('click', () => {
+    !mainWindow.isVisible() && mainWindow.show()
+  });
 });
 
-app.once("window-all-closed", () => app.quit());
+// app.once("window-all-closed", () => app.quit());
--- src/preload.ts
+++ src/preload.ts
@@ -2,6 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('si', {
   system: () => ipcRenderer.invoke('system'),
+  wifiConnections: () => ipcRenderer.invoke('wifiConnections'),
 })
 
 console.log("preloaded!");
